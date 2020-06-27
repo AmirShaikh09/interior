2776fa921de495a52e9a71f5d6e8f16dd4abe34b
Amir Shaikh <amirshaikh040@gmail.com>
2020-06-27 15:56:17 +0530

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,50 @@
+var i = 0;
+var images = [
+'assets/imgs/Designs/img (2).jpg',
+'/assets/imgs/Designs/img (3).jpg',
+'assets/imgs/Designs/img (4).jpg',
+'/assets/imgs/Designs/img (5).jpg',
+'/assets/imgs/Designs/img (6).jpg',
+'/assets/imgs/Designs/img (7).jpg',
+'/assets/imgs/Designs/img (8).jpg',
+'/assets/imgs/Designs/img (9).jpg',
+'/assets/imgs/Designs/img (10).jpg',
+'/assets/imgs/Designs/img (11).jpg',
+'/assets/imgs/Designs/img (12).jpg',
+'/assets/imgs/Designs/img (13).jpg',
+'/assets/imgs/Designs/img (14).jpg',
+'/assets/imgs/Designs/img (15).jpg',
+'/assets/imgs/Designs/img (16).jpg',
+'/assets/imgs/Designs/img (17).jpg',
+'/assets/imgs/Designs/img (18).jpg',
+'/assets/imgs/Designs/img (19).jpg',
+'/assets/imgs/Designs/img (20).jpg',
+'/assets/imgs/Designs/img (21).jpg'
+];
+                            
+function mySlide(param){
+  if(param === 'next'){
+    i++;
+    if(i === images.length){ 
+      i = images.length - 1; 
+    }
+  }
+  else{
+    i--;
+    if(i < 0){ 
+      i = 0; 
+    }
+  }
+  document.getElementById('slide').src = images[i];
+}
+
 // Script to open and close sidebar
 function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-  }
-   
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
-  
-  // Modal Image Gallery
-  function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-  }
-
-  
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
